2be6d77edf1373f34007c915b1d4fd9aab02968c
League Student <league@iMac.attlocal.net>
2020-02-13 18:28:49 -0800

    again

--- src/_01_Intro_To_Sockets/server/ServerGreeter.java
+++ src/_01_Intro_To_Sockets/server/ServerGreeter.java
@@ -5,20 +5,27 @@ import java.io.*;
 
 public class ServerGreeter extends Thread {
 	//1. Create an object of the ServerSocket class
-
+	ServerSocket ss;
 	public ServerGreeter() throws IOException {
 		//2. Initialize the ServerSocket object. In the parameters,
 		//   you must define the port at which the server will listen for connections.
-		
+		ss = new ServerSocket(8567);
+		ss.setSoTimeout(5000);
 		//*OPTIONAL* you can set a time limit for the server to wait by using the 
 		//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
 	}
 
-	public void run() {
+	public void run(){
 		//3. Create a boolean variable and initialize it to true.
-		
+		boolean sos = true;
 		//4. Make a while loop that continues looping as long as the boolean created in the previous step is true.
-			
+			while(sos) {
+				try {
+					
+				} catch (Exception e) {
+					
+				}
+			}
 			//5. Make a try-catch block that checks for two types Exceptions: SocketTimeoutException and IOException.
 			//   Put steps 8 - 15 in the try block.
 		
